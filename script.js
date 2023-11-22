56053606478b7cb3edafca7cbfe014ef889370e0
Daniel Rodrigues <danielrodrigues.dev@gmail.com>
2023-11-22 02:33:46 -0300

    add scroll suave e header fixo adaptativo ao scroll

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
+let itemMenu = document.querySelectorAll('a');
+
+
+// Detectar o scroll e alterar o header
+window.addEventListener("scroll", function() {
+    let header = document.querySelector('header');
+    header.classList.toggle('scroll', window.scrollY > 0);
+})
+
+
+// Destacar o item selecinado no menu
+itemMenu.forEach(item => {
+    item.addEventListener("click", function() {
+        document.querySelector(".ativo").classList.remove("ativo")
+        item.classList.add("ativo");
+    })
+})
+
+
+
 
 setInterval(function() {
     nextSlide();
